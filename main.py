0291c8af191f934d2d85e996cb4f79e0164042b1
shanalam <62662499+ShanAlam@users.noreply.github.com>
2022-10-19 00:50:40 +0000

    Added comments.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-# Write your code below this line 👇
+# Header
 print("Welcome, enjoy creating your new gamertag!")
 
+# Taking input from user
 colour = input("What is your favourite colour?")
 adjective = input("What is your favourite adjective?")
 animal = input("What is your favourite animal?")
 
+# Printing out results
 print("\n")
 print("The gamertag generated for you is:")
 print("The" + "_" + adjective + "_" + colour + "_" + animal)

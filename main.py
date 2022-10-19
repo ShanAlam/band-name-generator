f5ea2d2f924040c8721894c63612f5afb17012db
shanalam <62662499+ShanAlam@users.noreply.github.com>
2022-10-19 00:45:15 +0000

    Changed game into a gamertag generator and completed project.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # Write your code below this line 👇
-print("Hello World!")
+print("Welcome, enjoy creating your new gamertag!")
+
+colour = input("What is your favourite colour?")
+adjective = input("What is your favourite adjective?")
+animal = input("What is your favourite animal?")
+
+print("\n")
+print("The gamertag generated for you is:")
+print("The" + "_" + adjective + "_" + colour + "_" + animal)
+print("\n")
+print("Hope you like it :)")
